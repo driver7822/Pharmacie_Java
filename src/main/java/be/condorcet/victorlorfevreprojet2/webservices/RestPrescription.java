ed58ac53dcd20c0848056fde86580ce63b036068
Victor <78360638+driver7822@users.noreply.github.com>
2021-12-23 19:46:41 +0100

    Retrait du CrossOrigin
    mvn clean install : Ok

--- src/main/java/be/condorcet/victorlorfevreprojet2/webservices/RestPrescription.java
+++ src/main/java/be/condorcet/victorlorfevreprojet2/webservices/RestPrescription.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*",exposedHeaders = "*")
+//@CrossOrigin(origins = "*", allowedHeaders = "*",exposedHeaders = "*")
 @RestController
 @RequestMapping("/prescriptions")
 public class RestPrescription {
